2239b08358aac83c8ea436c968a75cceb206b440
sabnamparvinbristy <parvin.sabnam.002@gmail.com>
2025-05-31 02:43:35 +0600

    prisma monfo post modal

--- api/app.js
+++ api/app.js
@@ -14,9 +14,9 @@ app.use(express.json());
 app.use(cookieParser())
 
 // Register route
-app.use("/api/posts", postRoute);
-app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 
 
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,27 +1,15 @@
 import express from "express";
+import {verifyToken} from "../middleware/verifyToken.js";
+import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-    console.log("router works");
-     res.send("Test route is working!"); //Send a response
-});
 
 
-// router.post("/test", (req, res) => {
-//     console.log("router works");
-//     //  res.send("Test route is working!"); //Send a response
-// });
-
-// router.put("/test", (req, res) => {
-//     console.log("router works");
-//      res.send("Test route is working!"); //Send a response
-// });
-
-// router.delete("/test", (req, res) => {
-//     console.log("router works");
-//      res.send("Test route is working!"); //Send a response
-// });
-
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", verifyToken ,addPost);
+router.put("/:id", verifyToken ,updatePost);
+router.delete("/:id",verifyToken , deletePost);
 
 export default router;
