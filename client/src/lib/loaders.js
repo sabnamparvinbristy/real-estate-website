86b150bfdb4edeebfdcbf87fc23b98190a5c6108
sabnamparvinbristy <parvin.sabnam.002@gmail.com>
2025-06-01 00:56:23 +0600

    list fetch

--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -6,3 +6,12 @@ export const singlePageLoader = async ({request, params}) =>{
     return res.data;
 
 };
+
+export const listPageLoader = async ({request, params}) =>{
+    const query = request.url.split("?")[1]
+    
+    const res = await apiRequest("/posts?"+query);
+
+    return res.data;
+
+};
